a45a01b0d3a438877c9ad4d7acb93dbc2e581996
AlpacaTunnel <alpaca.tunnel@gmail.com>
2017-09-30 23:00:21 -0500

    remove unix socket forwarder

--- aiovpn/server.py
+++ aiovpn/server.py
@@ -3,67 +3,27 @@
 import aiohttp
 import asyncio
 import ipaddress
-import os
-import traceback
 from aiohttp import web
+import uvloop
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-from .helper import Arch, Command, Tunnel, TunSocketForwarder, byte2int, int2byte
+from .helper import Tunnel
 
 
-UNIX_SOCKET_PATH = '/tmp/aiovpn_server.socket'
 BROADCAST_MAC = bytes.fromhex('ffffffffffff')
 ALLOW_P2P = True
 
-US_READER, US_WRITER = None, None
 CLIENT_DICT = {}
-RECV_PKT_Q = asyncio.Queue()
+TUNFD = None
 
 
-async def us_connext(loop, unix_path):
-    global US_READER, US_WRITER, CLIENT_DICT, RECV_PKT_Q
+async def ws_send(send_q):
+    global CLIENT_DICT
 
-    while True:
-        try:
-            await asyncio.sleep(0.1)
-            fut = asyncio.open_unix_connection(unix_path, loop=loop)
-            reader, writer = await asyncio.wait_for(fut, timeout=0.1, loop=loop)
-
-            US_READER, US_WRITER = reader, writer
-            print('us_connext: connected to %s' % unix_path)
-            os.remove(unix_path)
-
-            break
-
-        except asyncio.TimeoutError:
-            print('us_connect: connect to %s timeout, retry...' % unix_path)
-            continue
-
-        except ConnectionRefusedError:
-            print('us_connect: connect to %s ConnectionRefusedError, retry...' % unix_path)
-            continue
-
-        except Exception as e:
-            err = traceback.format_exc()
-            print('us_connect: connect to %s exception: %s' % (unix_path, err))
-            raise(e)
+    print('ws_send: start to send packet')
 
     while True:
-        await asyncio.sleep(1000)
-
-
-async def us_read():
-    global US_READER, US_WRITER, CLIENT_DICT, RECV_PKT_Q
-
-    while US_READER is None:
-        await asyncio.sleep(0.1)
-
-    print('us_read: start to read unix socket packet')
-
-    while True:
-
-        len_mark = await US_READER.readexactly(4)
-        pkt_len = byte2int(len_mark)
-        packet = await US_READER.readexactly(pkt_len)
+        packet = await send_q.get()
 
         dst_mac = packet[0:6]
 
@@ -79,7 +39,7 @@ async def us_read():
 
 
 async def send_broad(packet):
-    global US_READER, US_WRITER, CLIENT_DICT, RECV_PKT_Q
+    global CLIENT_DICT
 
     for mac in list(CLIENT_DICT.keys()):
         # the dict may change during await
@@ -88,23 +48,6 @@ async def send_broad(packet):
             if ws.closed:
                 continue
             await ws.send_bytes(packet)
-
-
-async def us_write():
-    global US_READER, US_WRITER, CLIENT_DICT, RECV_PKT_Q
-
-    while US_WRITER is None:
-        await asyncio.sleep(0.1)
-
-    print('us_write: start to recv websockets packet')
-
-    while True:
-
-        packet = await RECV_PKT_Q.get()
-        pkt_len = len(packet)
-
-        US_WRITER.write(int2byte(pkt_len))
-        US_WRITER.write(packet)
 
 
 async def ws_recv_pkt(ws):
@@ -137,7 +80,8 @@ async def ws_recv_pkt(ws):
 
 
 async def websocket_handler(request):
-    global US_READER, US_WRITER, CLIENT_DICT, RECV_PKT_Q
+    global CLIENT_DICT, TUNFD
+    tun = TUNFD
 
     ws = web.WebSocketResponse(heartbeat=45)
     await ws.prepare(request)
@@ -163,7 +107,7 @@ async def websocket_handler(request):
     CLIENT_DICT[client_mac] = (ws, task)
     print('websocket_handler: add peer, MAC=%s' % client_mac.hex())
 
-    await RECV_PKT_Q.put(first_pkt)
+    tun.write(first_pkt)
 
     try:
         while True:
@@ -176,13 +120,13 @@ async def websocket_handler(request):
 
             if client_mac == src_mac:
                 if dst_mac == BROADCAST_MAC:
-                    await RECV_PKT_Q.put(packet)
+                    tun.write(packet)
                     await send_broad(packet)
                 elif dst_mac in CLIENT_DICT and ALLOW_P2P:
                     dst_ws, dst_task = CLIENT_DICT[dst_mac]
                     await dst_ws.send_bytes(packet)
                 else:
-                    await RECV_PKT_Q.put(packet)
+                    tun.write(packet)
 
             else:
                 msg = 'websocket_handler: client MAC changed from %s to %s, cancel the session' % (client_mac.hex(), src_mac.hex())
@@ -205,20 +149,27 @@ async def websocket_handler(request):
     return ws
 
 
+def tun_read(tun, send_q):
+    # Do NOT put while loop here! It will block other coroutines.
+    packet = tun.read(2048)
+    send_q.put_nowait(packet)
+
+
 def start_server(conf):
+    global TUNFD
 
-    tun = Tunnel(conf['name'], 'tap')
-    tun.IPv4 = ipaddress.IPv4Interface(conf['server_private_ip'])
-    tun.delete()
-    tun.add()
+    send_q = asyncio.Queue()
 
-    ts = TunSocketForwarder(UNIX_SOCKET_PATH, conf['name'])
-    ts.start()
+    tunif = Tunnel(conf['name'], 'tap')
+    tunif.IPv4 = ipaddress.IPv4Interface(conf['server_private_ip'])
+    tunif.delete()
+    tunif.add()
+    tun = tunif.open()
+    TUNFD = tun
 
     loop = asyncio.get_event_loop()
-    loop.create_task(us_connext(loop, UNIX_SOCKET_PATH))
-    loop.create_task(us_read())
-    loop.create_task(us_write())
+    loop.add_reader(tun.fileno(), tun_read, tun, send_q)
+    loop.create_task(ws_send(send_q))
 
     app = web.Application()
     app.router.add_get('/', websocket_handler)
